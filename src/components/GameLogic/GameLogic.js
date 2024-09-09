38ed87e6e02b4c80bc5bbc311a952186351446d7
Federico <federicofarias@outlook.com>
2024-09-09 19:19:04 -0300

    delete messageClass (not used)

--- src/components/GameLogic/GameLogic.js
+++ src/components/GameLogic/GameLogic.js
@@ -3,7 +3,6 @@ import { useState } from 'react';
 const useGameLogic = () => {
   const [guess, setGuess] = useState('');
   const [message, setMessage] = useState('Elegí un número del 1 al 20');
-  const [messageClass, setMessageClass] = useState('');
   const [score, setScore] = useState(20);
   const [highScore, setHighScore] = useState(0);
   const [gameOver, setGameOver] = useState(false);
@@ -13,11 +12,9 @@ const useGameLogic = () => {
   const handleGuess = (guessNumber) => {
     if (guessNumber < 1 || guessNumber > 20) {
       setMessage('Por favor, elige un número entre 1 y 20.');
-      setMessageClass('warning');
     } else {
       if (guessNumber === secretNumber) {
         setMessage(`¡Correcto! El número secreto es ${secretNumber}`);
-        setMessageClass('correct');
         setIsCorrect(true);
         if (score > highScore) {
           setHighScore(score);
@@ -25,11 +22,9 @@ const useGameLogic = () => {
         setGameOver(true);
       } else if (guessNumber < secretNumber) {
         setMessage('¡Muy bajo! Intenta un número más alto');
-        setMessageClass('incorrect');
         setScore(score - 1);
       } else {
         setMessage('¡Muy alto! Intenta un número más bajo');
-        setMessageClass('incorrect');
         setScore(score - 1);
       }
 
@@ -44,7 +39,6 @@ const useGameLogic = () => {
   const resetGame = () => {
     setGuess('');
     setMessage('Elegí un número del 1 al 20');
-    setMessageClass('');
     setScore(20);
     setGameOver(false);
     setSecretNumber(Math.floor(Math.random() * 20) + 1);
@@ -55,7 +49,6 @@ const useGameLogic = () => {
     guess,
     setGuess,
     message,
-    messageClass,
     score,
     highScore,
     gameOver,
